12c79a43ff8479874af8054a8a65c60fae362948
minghui3 <s10257917@connect.np.edu.sg>
2024-02-03 19:39:05 +0800

    wasd

---
+++ leaderboard.js
@@ -0,0 +1,15 @@
+var weeklyDate = new Date("Feb 10, 2024 00:00:00");
+var x = setInterval(function(){
+    var d = new Date().getTime();
+    var distance = weeklyDate - d;
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("countDown").textContent = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    if (distance < 0){
+        
+    }
+},1000)
