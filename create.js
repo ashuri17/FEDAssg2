1a2d9c2fae6aaa2863a3b0a5882acac578a4c9a5
ashuri <s10257765@connect.np.edu.sg>
2024-02-07 15:42:38 +0800

    ensured game works properly

--- create.js
+++ create.js
@@ -1,55 +1,62 @@
-    document.addEventListener("DOMContentLoaded",function(){
-        const APIKEY = "65bf3321b4ef994fc436669e"
-        document.getElementById("final-create").addEventListener("click", function(e){
-            e.preventDefault();
-            let userName = document.getElementById("create-username").value;
-            let email = document.getElementById("create-email").value;
-            let password = document.getElementById("create-password").value;
-            if (password.length < 8 && userName.length < 12){
-                alert("Password should be at least 8 characters long!");
-                document.getElementById("create-username").value = "";
-                document.getElementById("create-email").value = "";
-                document.getElementById("create-password").value = "";
+document.addEventListener("DOMContentLoaded",function(){
+    const APIKEY = "65bf3321b4ef994fc436669e"
+    document.getElementById("final-create").addEventListener("click", function(e){
+        e.preventDefault();
+        let userName = document.getElementById("create-username").value;
+        let email = document.getElementById("create-email").value;
+        let password = document.getElementById("create-password").value;
+        if (password.length < 8 && userName.length < 12){
+            alert("Password should be at least 8 characters long!");
+            document.getElementById("create-username").value = "";
+            document.getElementById("create-email").value = "";
+            document.getElementById("create-password").value = "";
+        }
+        else{
+            let today = new Date("01/01/2024");
+            var year = today.getFullYear();
+            var month = ('0' + (today.getMonth()+1)).slice(-2);
+            var days = ('0' + today.getDate()).slice(-2);
+            formattedDate = days + '/' + month + '/' + year;
+            let userData = {
+                "userName": userName,
+                "userEmail": email,
+                "userPassword": password,
+                "highestStreak": 0,
+                "dailyScore": 0,
+                "totalScore": 0,
+                "differenceDaily": 0,
+                "date": formattedDate
             }
-            else{
-                let userData = {
-                    "userName": userName,
-                    "userEmail": email,
-                    "userPassword": password,
-                    "highestStreak": 0,
-                    "dailyScore": 0,
-                    "totalScore": 0,
+            localStorage.setItem("userNameKey",userName);
+            let settings = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-apikey": APIKEY,
+                    "Cache-Control": "no-cache"
+                },
+                body: JSON.stringify(userData),
+            }
+            fetch("https://fedassg2-7a05.restdb.io/rest/user-fed",settings)
+            .then((response)=>{
+                if (!response.ok){
+                    alert("Username has been taken!");
+                    document.getElementById("create-username").value = "";
+                    document.getElementById("create-email").value = "";
+                    document.getElementById("create-password").value = "";
+                    throw new Error("Something went wrong...");
                 }
-                localStorage.setItem("userNameKey",userName);
-                let settings = {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "x-apikey": APIKEY,
-                        "Cache-Control": "no-cache"
-                    },
-                    body: JSON.stringify(userData),
+                else{
+                    return response.json();
                 }
-                fetch("https://fedassg2-7a05.restdb.io/rest/user-fed",settings)
-                .then((response)=>{
-                    if (!response.ok){
-                        alert("Username has been taken!");
-                        document.getElementById("create-username").value = "";
-                        document.getElementById("create-email").value = "";
-                        document.getElementById("create-password").value = "";
-                        throw new Error("Something went wrong...");
-                    }
-                    else{
-                        return response.json();
-                    }
-                })
-                .then ((data) =>{
-                    console.log(data);
-                    window.location.href = "homepage.html"
-                })
-                .catch((error) => {
-                    console.error("Error fetching data:", error);
-                });
-            } 
-        })
+            })
+            .then ((data) =>{
+                console.log(data);
+                window.location.href = "homepage.html"
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+        } 
     })
+})
--- script.js
+++ script.js
@@ -3,15 +3,25 @@ document.addEventListener("DOMContentLoaded",(event) => {
 })
 const APIKEY = "65bf3321b4ef994fc436669e";	
 const countryFlags = {}
-let answer;
-let highestStreak = 0;
+let answer;                         
+let highestStreak;                  
 let gameStreak = 0;
-let dailyScore = 0;
+let dailyScore;                  
+let differenceDaily;
+let totalScore;
+let userDate;
+let userID;
+
+let now = new Date().toLocaleDateString('en-GB') //for resetting of dailyScore and differenceDaily values
 document.getElementById("userStreak").textContent = gameStreak;
-document.getElementById("userDailyScore").textContent = dailyScore;
-document.querySelector('form').addEventListener("click", function (e){
+document.querySelector('form').addEventListener("click", function (e){ //disable form default
     e.preventDefault();
 })
+let playerName = localStorage.getItem("userNameKey"); //grab the player name from local storage
+
+
+//Instantiating of flags.png along with its corresponding name
+
 function createFlags() {
     fetch("https://restcountries.com/v3.1/all")
     .then ((response) => {
@@ -30,10 +40,11 @@ function createFlags() {
             countryFlags[flagImage] = flagName;
         }
         console.log(countryFlags);      
-        randomCountry()  
+        randomCountry();
     })   
 }
 
+//Generate random country flag along with its answer
 function randomCountry(){
     let countryKeys = Object.keys(countryFlags);
     let randomCountryFlag = countryKeys[Math.floor(Math.random() * countryKeys.length)];
@@ -41,31 +52,114 @@ function randomCountry(){
     answer = countryFlags[randomCountryFlag];
     console.log(answer);
 }
+//check if user input is the same as the name of the country flag
 function checkName(){
     let userInput = document.getElementById("userCountry").value;
-    if (userInput.toLowerCase() === "help"){
-        console.log(answer);
-    }
     if (userInput.toLowerCase() === answer.toLowerCase()){
         randomCountry();
         gameStreak++;
-        dailyScore += 10;
-        console.log(gameStreak);
-        console.log(dailyScore);
+        dailyScore += 10
+        if(dailyScore >= 100){
+            dailyScore = 100;
+            let today = new Date()
+            var year = today.getFullYear();
+            console.log(year);
+            var month = ('0' + (today.getMonth()+1)).slice(-2);
+            console.log(month);
+            var days = ('0' + today.getDate()).slice(-2);
+            userDate = days + '/' + month + '/' + year;
+        }
+
     }
+    // if user input does not match the name of the country flag
     else{
         if (highestStreak < gameStreak){
-            highestStreak = gameStreak;
+            highestStreak = gameStreak;     //modify user's highest streak
         }
-        const popupWindow = document.querySelector(".popup");
+        const popupWindow = document.querySelector(".popup");   
         const fadedBg = document.querySelector("#faded-bg")
-        popupWindow.style.display = 'flex';
+        popupWindow.style.display = 'flex';     //enable popups when player loses
         fadedBg.style.display = 'block';
-        document.getElementById("gameStreak").textContent = gameStreak;
-        gameStreak = 0;
+        document.getElementById("gameStreak").textContent = gameStreak; //display streak of current game session
+        if (differenceDaily == 0){
+            totalScore += dailyScore;
+        }
+        else{
+            
+            totalScore += (dailyScore - differenceDaily); //ensures that totalScore is tabulated correctly
+        } 
+        differenceDaily = dailyScore
+        gameStreak = 0; 
+        updatePlayer();
     }
     document.getElementById("userCountry").value = "";
     document.getElementById("userStreak").textContent = gameStreak;
     document.getElementById("userDailyScore").textContent = dailyScore;
 }
+// get player's stats from DB
+function getPlayer(){
+    let settings = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "x-apikey": APIKEY,
+            "Cache-Control": "no-cache"
+        },
+    }
+    fetch(`https://fedassg2-7a05.restdb.io/rest/user-fed?q={"userName":"${playerName}"}`,settings)
+    .then((response)=>{
+        if (!response.ok){
+            throw new Error("Something went wrong...");
+        }
+        else{
+            return response.json();
+        }
+    })
+    .then ((data) =>{
+        if (data[0].userDate === now){
+            dailyScore = 0
+            differenceDaily = 0
+        }
+        else{
+            dailyScore = data[0].dailyScore;
+            differenceDaily = data[0].differenceDaily;
+        }
+        highestStreak = data[0].highestStreak;
+        totalScore = data[0].totalScore;
+        userID = data[0]._id;
+        document.getElementById("userDailyScore").textContent = dailyScore;
+    })
+}
+// Update player stats
+function updatePlayer(){
+    let playerData = {
+        "highestStreak": highestStreak,
+        "dailyScore": dailyScore,
+        "totalScore": totalScore,
+        "differenceDaily": differenceDaily,
+        "date": userDate
+    }
+    let settings = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "x-apikey": APIKEY,
+            "Cache-Control": "no-cache"
+        },
+        body: JSON.stringify(playerData)
+    }
+    fetch(`https://fedassg2-7a05.restdb.io/rest/user-fed/${userID}`,settings)
+    .then((response)=>{
+        if (!response.ok){
+            throw new Error("Something went wrong...");
+        }
+        else{
+            return response.json();
+        }
+    })
+    .then ((data) =>{
+        console.log(data);
+    })
+}
 createFlags();
+getPlayer();
